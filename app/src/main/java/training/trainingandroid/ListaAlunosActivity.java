130413170f59906d62805743f437ac975d98f98a
MEMT\loraine.duarte <loraine.duarte@memt.com.br>
2018-07-25 17:17:03 -0300

    Aula 01 - Melhorando a lista

--- app/src/main/java/training/trainingandroid/ListaAlunosActivity.java
+++ app/src/main/java/training/trainingandroid/ListaAlunosActivity.java
@@ -2,6 +2,8 @@ package training.trainingandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        String [] alunos= {"Daniel","Ronaldo", "Jeferson", "Felipe"};
+        ListView listaAlunos = findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter <String> (this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
     }
 }
